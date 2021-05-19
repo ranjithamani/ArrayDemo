8dbb1ec5716464a66488015a5aad9db549ac77d5
user <ranjitham829@gmail.com>
2021-05-19 12:37:22 +0530

    adding ArrayDemo

---
+++ ArrayDemo.java
@@ -0,0 +1,16 @@
+class ArrayDemo{
+public static void main(String[]  a){
+int evenNumbers[]={ 2,4,6,8,12 };
+System.out.println(evenNumbers[4]);
+System.out.println("size of evenNumbers " +evenNumbers.length);
+
+int []oddNumbers = new int[10];
+System.out.println("size of oddNumbers " +oddNumbers.length);
+System.out.println(oddNumbers[0]);
+oddNumbers[0]=10;
+System.out.println(oddNumbers[0]);
+
+}
+
+
+}
